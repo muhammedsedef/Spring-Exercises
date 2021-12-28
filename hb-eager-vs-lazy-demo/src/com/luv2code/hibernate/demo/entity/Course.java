6815a7c80f6223ce86cc657e06eac466d336fd18
muhammedsedef <muhammetsedef34@gmail.com>
2021-12-28 18:37:23 +0300

    finished eager vs lazy demo

---
+++ hb-eager-vs-lazy-demo/src/com/luv2code/hibernate/demo/entity/Course.java
@@ -0,0 +1,68 @@
+package com.luv2code.hibernate.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    // define our fields
+
+    // define constructors
+
+    // define getter and setter
+
+    // define toString
+
+    // annotate fields
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+
+    public Course()  {}
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
